fcb57bfe0678956ca1fd51a5db6e135bfd4e4446
ayaamohammedali <162024621+ayaamohammedali@users.noreply.github.com>
2024-05-03 15:30:23 +0300

    Add files via upload

---
+++ C program to find cube of an integer number using two different methods/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int  main()
+{
+
+    int a,cube;
+
+    printf("Enter any integer number: ");
+    scanf("%d",&a);
+    //calculating cube
+    cube = (a*a*a);
+    printf("CUBE is: %d\n",cube);
+
+    return 0;
+}
